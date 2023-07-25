1ce6634f947e5928d3d5203a21db12f71fbc6301
faint <faintcsr@gmail.com>
2023-07-25 19:03:06 +0200

    cleaned up props definitions with spread operator and prop accessibility with destructuring

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
 import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+class Expense {
+  constructor(id, title, amount, date) {
+    this.id = id;
+    this.title = title;
+    this.amount = amount;
+    this.date = date;
+  }
+}
 
+const expenses = [
+  new Expense('e1', 'Toilet Paper', 94.12, new Date(2020, 7, 14)),
+  new Expense('e2', 'New TV', 799.49, new Date(2021, 2, 12)),
+  new Expense('e3', 'Car Insurance', 294.67, new Date(2021, 2, 28)),
+  new Expense('e4', 'New Desk (Wooden)', 450, new Date(2021, 5, 12))
+];
+
+function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
+        {...expenses[0]}
+      />
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
+        {...expenses[1]}
+      />
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+        {...expenses[2]}
+      />
       <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+        {...expenses[3]}
+      />
     </div>
   );
 }
